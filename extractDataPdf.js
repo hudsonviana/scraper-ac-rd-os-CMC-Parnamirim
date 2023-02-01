cf19dbd913beec0b79883d5b131711bc73bf2aff
hudsonviana <hudson1206@gmail.com>
2023-02-01 00:57:51 -0300

    Ajustes na variável resultado_recurso

--- extractDataPdf.js
+++ extractDataPdf.js
@@ -3,9 +3,9 @@ const pdfParse = require('pdf-parse');
 const xlsx = require('xlsx');
 const path = require('path');
 
-const pathPdf = 'data3';
+const pathPdf = 'data';
 const files = fs.readdirSync(pathPdf);
-let acordaos = [];
+let judgments = [];
 
 async function exportResultsToExcel() {
   const sheetName = 'acordaos_cmc_parn';
@@ -16,7 +16,7 @@ async function exportResultsToExcel() {
   const filePath = path.join(__dirname, fileName);
 
   var workbook = xlsx.utils.book_new();
-  var worksheet = xlsx.utils.json_to_sheet(acordaos);
+  var worksheet = xlsx.utils.json_to_sheet(judgments);
   xlsx.utils.book_append_sheet(workbook, worksheet, sheetName);
   try {
     xlsx.writeFile(workbook, filePath, { bookType: 'xlsx' });
@@ -25,83 +25,83 @@ async function exportResultsToExcel() {
   }
 }
 
-async function extractDataFromPdf() {
-  await Promise.all(files.map(async (file) => {
+async function extractData(dataText, file) {
+  console.log('Lendo Arquivo: ', file);
 
-    let acordao = await new Promise(async (resolve, reject) => {
-      try {
-        await pdfParse(`${pathPdf}/${file}`).then((data) => {
+  let nome_arquivo = file;
 
-          const dataText = data.text;
-          // console.log(dataText);
+  let num_processo = /PROCESSO(.*?)\n/gi.exec(dataText)[1].trim().replace(/[^\d]/g, '').substring(0, 11);
 
-          console.log('Lendo Arquivo: ', file);
+  let num_acordao = 'N/D';
+  const judgmentTargets = ['ACÓRDÃO', 'ACORDÃO', 'ACORDAO', 'A C Ó R D Ã O', 'O N. º'];
+  for (const judgmentTarget of judgmentTargets) {
+    if (dataText.includes(judgmentTarget)) {
+      num_acordao =
+        new RegExp(`${judgmentTarget}(.*)`, 'gi').exec(dataText)[1].trim().replace(/[^0-9\/]/g, '') ||
+        new RegExp(`${judgmentTarget}(.*)`, 'gs').exec(dataText)[1].trim().replace(/[^0-9\/]/g, '');
+      break;
+    }
+  }
 
-          let nome_arquivo = file;
+  let tributo = 'N/D';
+  const taxConditions = [
+    { condition: dataText.includes('IPTU') || dataText.includes('71/2013'), tributo: 'IPTU' },
+    { condition: dataText.includes('ITIV') || dataText.includes('ITBI') || dataText.includes('TRANSMISSÃO INTER'), tributo: 'ITBI' },
+    { condition: dataText.includes('ISSQN') || dataText.includes('ISS  SUBSTITUTO') || dataText.includes('SERVIÇOS  DE  QUALQUER  NATUREZA') || dataText.includes('ISS SUBSTITUTO') || dataText.includes('ISS.') || dataText.includes(' ISS ') || dataText.includes('QN – SUBSTITUTO'), tributo: 'ISSQN' },
+    { condition: dataText.includes('DMS') || dataText.includes('DECLARAÇÃO MENSAL DE SERVIÇOS') || dataText.includes('OBRIGAÇÃO ACESSÓRIA'), tributo: 'OBRIGAÇÃO ACESSÓRIA' },
+    { condition: dataText.toLowerCase().includes('taxa ') || dataText.includes('ALVARÁ'), tributo: 'TAXAS' }
+  ];
+  taxConditions.forEach(c => {
+    if (c.condition) {
+      tributo = c.tributo;
+      return;
+    }
+  });
 
-          let num_processo = /PROCESSO(.*?)\n/gi.exec(dataText)[1].trim().replace(/[^\d]/g, '').substring(0, 11);
+  // let resultado_recurso = dataText.toLowerCase().includes('improvido') ? 'IMPROVIDO' :
+  //   dataText.toLowerCase().includes('desprovido') ? 'IMPROVIDO' :
+  //   dataText.toLowerCase().includes('parcialmente provido') ? 'PARCIALMENTE PROVIDO' :
+  //   dataText.toLowerCase().includes('provido') ? 'PROVIDO' : 'NÃO CONHECIDO';
 
-          let num_acordao = 'N/D';
-          const acordaosArray = ['ACÓRDÃO', 'ACORDÃO', 'ACORDAO', 'A C Ó R D Ã O', 'O N. º'];
-          for (const acordaoEl of acordaosArray) {
-            if (dataText.includes(acordaoEl)) {
-              num_acordao = 
-                new RegExp(`${acordaoEl}(.*)`, 'gi').exec(dataText)[1].trim().replace(/[^0-9\/]/g, '') || 
-                new RegExp(`${acordaoEl}(.*)`, 'gs').exec(dataText)[1].trim().replace(/[^0-9\/]/g, '');
-              break;
-            }
-          }
+  // const appealResultTargets = /IMPROVIDO|DESPROVIDO|PARCIALMENTE PROVIDO|PROVIDO/gi;
+  // let resultado_recurso = appealResultTargets.test(dataText) ? 
+  // dataText.match(appealResultTargets)[0] === 'DESPROVIDO' ? 'IMPROVIDO' : dataText.toUpperCase().match(appealResultTargets)[0] : 'NÃO CONHECIDO';
 
-          let tributo = 'N/D';
-          const conditionsTrib = [
-            { condition: dataText.includes('IPTU') || dataText.includes('71/2013'), tributo: 'IPTU' },
-            { condition: dataText.includes('ITIV') || dataText.includes('ITBI') || dataText.includes('TRANSMISSÃO INTER'), tributo: 'ITBI' },
-            { condition: dataText.includes('ISSQN') || dataText.includes('ISS  SUBSTITUTO') || dataText.includes('SERVIÇOS  DE  QUALQUER  NATUREZA') || dataText.includes('ISS SUBSTITUTO') || dataText.includes('ISS.') || dataText.includes(' ISS ') || dataText.includes('QN – SUBSTITUTO'), tributo: 'ISSQN' },
-            { condition: dataText.includes('DMS') || dataText.includes('DECLARAÇÃO MENSAL DE SERVIÇOS') || dataText.includes('OBRIGAÇÃO ACESSÓRIA'), tributo: 'OBRIGAÇÃO ACESSÓRIA' },
-            { condition: dataText.toLowerCase().includes('taxa ') || dataText.includes('ALVARÁ'), tributo: 'TAXAS' }
-          ];
+  const appealResultTargets = /IMPROVIDO|DESPROVIDO|PARCIALMENTE PROVIDO|PROVIDO/gi;
+  let resultado_recurso = appealResultTargets.test(dataText) ?
+    dataText.toUpperCase().match(appealResultTargets)[0].replace(/DESPROVIDO/gi, 'IMPROVIDO') : 'NÃO CONHECIDO';
 
-          conditionsTrib.forEach(c => {
-            if (c.condition) {
-              tributo = c.tributo;
-              return;
-            }
-          });
+  // let tipo_recurso = dataText.toLowerCase().includes('voluntário' || 'voluntario') ? 'RECURSO VOLUNTÁRIO' : 'RECURSO DE OFÍCIO';
 
-          // Pegar também o 'PARCIALMENTE PROVIDO'
-          let resultado = dataText.toLowerCase().includes('improvido') ? 'IMPROVIDO' :
-            dataText.toLowerCase().includes('desprovido') ? 'IMPROVIDO' :
-              dataText.toLowerCase().includes('provido') ? 'PROVIDO' : 'NÃO CONHECIDO';
+  let tipo_recurso = /RECURSO VOLUNT(ÁRIO|ARIO)/i.test(dataText) ? 'RECURSO VOLUNTÁRIO' : 'RECURSO DE OFÍCIO';
 
-          // appeal
-          let tipo_recurso = dataText.toLowerCase().includes('voluntário' || 'voluntario') ? 'RECURSO VOLUNTÁRIO' : 'RECURSO DE OFÍCIO';
+  let data_julgamento = dataText.toLowerCase().includes('data de julgamento') ?
+    /Julgamento\:(.*?)\./gi.exec(dataText)[1].trim() :
+    dataText.includes('Data do julgamento') ?
+      /julgamento\:(.*?)\./gi.exec(dataText)[1].trim() :
+      /Parnamirim,\s(.*?)\./gi.exec(dataText)[1].trim();
 
-          let data_julgamento = dataText.toLowerCase().includes('data de julgamento') ?
-            /Julgamento\:(.*?)\./gi.exec(dataText)[1].trim() :
-            dataText.includes('Data do julgamento') ?
-              /julgamento\:(.*?)\./gi.exec(dataText)[1].trim() :
-              /Parnamirim,\s(.*?)\./gi.exec(dataText)[1].trim();
-
-          resolve({
-            num_processo,
-            num_acordao,
-            tributo,
-            tipo_recurso,
-            resultado,
-            data_julgamento,
-            nome_arquivo
-          });
-        }); // aqui
-      } catch (error) {
-        reject(console.log('Erro no arquivo:', file, '->', error));
-      }
-    });
-
-    acordaos.push(acordao);
-  }));
-
-  console.log(acordaos);
-  // exportResultsToExcel();
+  judgments.push({
+    num_processo,
+    num_acordao,
+    tributo,
+    tipo_recurso,
+    resultado_recurso,
+    data_julgamento,
+    nome_arquivo
+  });
 }
 
-extractDataFromPdf();
+async function getDataTextFromPdf() {
+  await Promise.all(files.map(async (file) => {
+    await pdfParse(`${pathPdf}/${file}`).then(async (data) => {
+      const dataText = data.text;
+      // console.log(dataText);
+      await extractData(dataText, file);
+    });
+  }));
+  console.log(judgments);
+  await exportResultsToExcel();
+}
+
+getDataTextFromPdf();
